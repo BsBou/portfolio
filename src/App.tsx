9b2c098a1816a9f1fd08d66743bb700bdc86ce5f
Borhan Boulandier <borhan.boulandier@gmail.com>
2025-01-19 19:26:05 +0000

    feat: init vite project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Borhan Boulandier</h1>
+
+      <p>Hi, I'm Borhan I like working on hard problems with great people.</p>
+      <p>
+        Backend engineer at <a href="http://passentry.com">PassEntry</a>
+      </p>
+      <p>
+        Lecturer at <a href="https://www.lewagon.com/london">Le Wagon London</a>
+      </p>
+    </>
+  );
+}
+
+export default App;
